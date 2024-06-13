02cce09cef9bd6b24d8b5cf184e1cf19bd677dda
lfuis201 <lmamania@ulasalle.edu.pe>
2024-06-13 13:07:51 -0500

    init proyect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './pages/homePage/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+
+
+];
